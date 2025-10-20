79e50d51aab4bbc810c91200866cd8afbbdd8f7d
EddyZard <eddy.zard496@gmail.com>
2025-10-20 20:37:18 -0300

    Adição funcionando!!!

---
+++ aplicacao.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const porta = 3000;
+
+// API que recebe dois números e retorna a soma
+app.get('/adicao', (req, res) => {
+    const num1 = parseFloat(req.query.n1);
+    const num2 = parseFloat(req.query.n2);
+    const resultado = num1 + num2;
+    res.send(`Resultado: ${resultado}`);
+});
+// Pra executar o cálculo, você adiciona isso na URL:
+// http://localhost:3000/adicao?n1=3&n2=2
+// Nesse exemplo estamos atribuindo 3 à n1 e 2 à n2, o resultado deve ser 5
+
+
+// Outras APIs
+// subtracao,
+// multiplicacao
+
+app.listen(porta, () => {
+    console.log(`Servidor rodando na porta ${porta}`);
+});
