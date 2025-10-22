7b6d6664fc893113c7957bec831961c0a849a114
EddyZard <eddy.zard496@gmail.com>
2025-10-21 22:16:16 -0300

    Anotações adicionadas

--- aplicacao.test.js
+++ aplicacao.test.js
@@ -3,21 +3,25 @@ const app = require('./aplicacao'); //importa o aplicacao.js
 
 describe('Testando as API´s de operações matemáticas', () => {
 
+//Testa Soma
     test('Verificar a soma de dois números', async() => {
         const response = await request (app).get('/adicao?n1=2&n2=5');
         expect(response.text).toBe('Resultado: 7');
     });
 
+//Testa Subtração
     test('Verificar a subtração de dois números', async() => {
         const response = await request (app).get('/subtracao?n1=5&n2=2');
         expect(response.text).toBe('Resultado: 3');
     });
 
+//Testa Divisão
     test('Verificar a divisão de dois números', async() => {
         const response = await request (app).get('/divisao?n1=10&n2=5');
         expect(response.text).toBe('Resultado: 2');
     });
 
+//Testa Multiplicação
     test('Verificar a multiplicação de dois números', async() => {
         const response = await request (app).get('/multiplicacao?n1=2&n2=5');
         expect(response.text).toBe('Resultado: 10');
